ef0007786f0784bfd3320f5102ece2513b4f6d3f
Li-Xian <twbrandon7@gmail.com>
2025-02-08 11:44:27 +0000

    update docstrings for TimesFmIterableDataset

--- src/timesfm/data_loader.py
+++ src/timesfm/data_loader.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""TF dataloaders for general timeseries datasets.
+"""Dataloaders for general timeseries datasets.
 
 The expected input format is csv file with a datetime index.
 """
--- src/timesfm/pytorch_timesfm_iterable_dataset.py
+++ src/timesfm/pytorch_timesfm_iterable_dataset.py
@@ -1,3 +1,21 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""PyTorch dataset adapter for TimesFM.
+
+This dataset is designed to be used with `timesfm.data_loader.TimeSeriesdata`.
+"""
+
 from typing import Callable, Generator, Tuple
 
 import numpy as np
@@ -6,6 +24,12 @@ from torch.utils.data import IterableDataset
 
 
 class TimesFmIterableDataset(IterableDataset):
+  """PyTorch dataset adapter for TimesFM.
+
+  Args:
+    generator_fn: A function that returns a generator that yields tuples of
+      numpy arrays. For the expected format of the tuples, see `timesfm.data_loader.TimeSeriesdata.train_gen()`.
+  """
   def __init__(
     self,
     generator_fn: Callable[
@@ -29,6 +53,7 @@ class TimesFmIterableDataset(IterableDataset):
     self._generator_fn = generator_fn
 
   def _generator_wrapper(self, worker_id: int, num_workers: int):
+    """Wraps the generator to yield data only for the current worker."""
     i = 0
     for data in self._generator_fn():
       if i % num_workers == worker_id:
@@ -36,6 +61,7 @@ class TimesFmIterableDataset(IterableDataset):
       i += 1
 
   def __iter__(self):
+    """Returns an iterator over the dataset."""
     worker_info = torch.utils.data.get_worker_info()
 
     if worker_info is None:
