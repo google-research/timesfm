9e731edc319516e1b55dfd5ac64ad82d716d0c55
Yichen Zhou <yichenzhou@google.com>
2025-09-03 06:11:22 +0000

    init commit of torch files

--- src/timesfm/__init__.py
+++ src/timesfm/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Google LLC
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,25 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""TimesFM init file."""
-
-print(
-    " See https://github.com/google-research/timesfm/blob/master/README.md for updated APIs."
-)
-from timesfm.timesfm_base import (
-    freq_map,
-    TimesFmCheckpoint,
-    TimesFmHparams,
-    TimesFmBase,
-)
-import sys
-
-try:
-    from timesfm.timesfm_jax import TimesFmJax as TimesFm
-    from timesfm import data_loader
-
-    print(f"Loaded Jax TimesFM, likely because python version is {sys.version}.")
-except Exception as _:
-    from timesfm.timesfm_torch import TimesFmTorch as TimesFm
-
-    print(f"Loaded PyTorch TimesFM, likely because python version is {sys.version}.")
