36d7f68a27e58f3eec7715dfb9cd725f4a488aaf
Yichen Zhou <yichenzhou@google.com>
2025-09-04 05:43:31 +0000

    init pytorch API

---
+++ src/timesfm/configs.py
@@ -0,0 +1,78 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Abstract configs for TimesFM layers."""
+
+import dataclasses
+from typing import Literal
+
+
+@dataclasses.dataclass(frozen=False)
+class ForecastConfig:
+  """Options for forecasting."""
+
+  max_context: int = 0
+  max_horizon: int = 0
+  normalize_inputs: bool = False
+  window_size: int = 0
+  per_core_batch_size: int = 1
+  use_continuous_quantile_head: bool = False
+  infer_is_positive: bool = True
+  fix_quantile_crossing: bool = False
+  return_backcast: bool = False
+
+
+@dataclasses.dataclass(frozen=True)
+class ResidualBlockConfig:
+  """Framework-agnostic config for a residual block."""
+
+  input_dims: int
+  hidden_dims: int
+  output_dims: int
+  use_bias: bool
+  activation: Literal["relu", "swish", "none"]
+
+
+@dataclasses.dataclass(frozen=True)
+class RandomFourierFeaturesConfig:
+  """Framework-agnostic config for random fourier features."""
+
+  input_dims: int
+  output_dims: int
+  projection_stddev: float
+  use_bias: bool
+
+
+@dataclasses.dataclass(frozen=True)
+class TransformerConfig:
+  """Framework-agnostic config for a transformer."""
+
+  model_dims: int
+  hidden_dims: int
+  num_heads: int
+  attention_norm: Literal["rms"]
+  feedforward_norm: Literal["rms"]
+  qk_norm: Literal["rms", "none"]
+  use_bias: bool
+  use_rotary_position_embeddings: bool
+  ff_activation: Literal["relu", "swish", "none"]
+
+
+@dataclasses.dataclass(frozen=True)
+class StackedTransformersConfig:
+  """Framework-agnostic config for a stacked transformers."""
+
+  num_layers: int
+  transformer: TransformerConfig
+
