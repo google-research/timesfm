c85a3dd7b72c8f5fea7d72a1b214db228462fb7d
Bortlesboat <bortstheboat@gmail.com>
2026-04-08 18:29:08 -0400

    fix: retain compiled torch model after load

--- src/timesfm/timesfm_2p5/timesfm_2p5_torch.py
+++ src/timesfm/timesfm_2p5/timesfm_2p5_torch.py
@@ -81,13 +81,6 @@ class TimesFM_2p5_200M_torch_module(nn.Module):
     tensors = load_file(path)
     self.load_state_dict(tensors, strict=True)
     self.to(self.device)
-    torch_compile = True
-    if "torch_compile" in kwargs:
-      torch_compile = kwargs["torch_compile"]
-    if torch_compile:
-      print("Compiling model...")
-      self = torch.compile(self)
-
     self.eval()
 
   def forward(
@@ -333,9 +326,10 @@ class TimesFM_2p5_200M_torch(
 
     logging.info("Loading checkpoint from: %s", model_file_path)
     # Load the weights into the model.
-    instance.model.load_checkpoint(
-      model_file_path, torch_compile=instance.torch_compile
-    )
+    instance.model.load_checkpoint(model_file_path)
+    if instance.torch_compile:
+      instance.model = torch.compile(instance.model)
+      instance.model.eval()
     return instance
 
   def _save_pretrained(self, save_directory: Union[str, Path]):
